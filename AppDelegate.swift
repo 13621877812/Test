6bfc1bd8eb68970bdd4e7d037c1688c96fd7bc9e
sun <1549057151@qq.com>
2018-07-14 23:25:46 +0800

    7

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -13,7 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 共和国姑获鸟 
-
+几级了就
+    后悔何及
+    鞍山市
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
